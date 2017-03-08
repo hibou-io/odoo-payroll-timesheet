3ef4e1d415a3e32206cff46c46384219409bb474
Jared Kipe <jared@hibou.io>
2017-03-08 09:32:33 -0800

    Fix: this allows the Payslip Batch feature to work correctly

--- hr_payroll_timesheet/hr_payslip.py
+++ hr_payroll_timesheet/hr_payslip.py
@@ -7,30 +7,42 @@ class HrPayslip(models.Model):
 
     @api.model
     def get_worked_day_lines(self, contract_ids, date_from, date_to):
-        self.ensure_one()
+        def create_empty_worked_lines(employee_id, contract_id, date_from, date_to):
+            attendance = {
+                'name': 'Timesheet Attendance',
+                'sequence': 10,
+                'code': 'ATTN',
+                'number_of_days': 0.0,
+                'number_of_hours': 0.0,
+                'contract_id': contract_id,
+            }
 
-        attendances = {
-            'name': 'Timesheet Attendance',
-            'sequence': 10,
-            'code': 'ATTN',
-            'number_of_days': 0.0,
-            'number_of_hours': 0.0,
-            'contract_id': self.contract_id.id,
-        }
+            valid_days = [
+                ('sheet_id.employee_id', '=', employee_id),
+                ('sheet_id.state', '=', 'done'),
+                ('sheet_id.date_from', '>=', date_from),
+                ('sheet_id.date_to', '<=', date_to),
+            ]
+            return attendance, valid_days
 
-        valid_days = [
-            ('sheet_id.employee_id', '=', self.employee_id.id),
-            ('sheet_id.state', '=', 'done'),
-            ('sheet_id.date_from', '>=', date_from),
-            ('sheet_id.date_to', '<=', date_to),
-        ]
+        attendances = []
 
-        for day in self.env['hr_timesheet_sheet.sheet.day'].search(valid_days):
-            if day.total_attendance >= 0.0:
-                attendances['number_of_days'] += 1
-                attendances['number_of_hours'] += day.total_attendance
+        for contract in self.env['hr.contract'].browse(contract_ids):
+            attendance, valid_days = create_empty_worked_lines(
+                contract.employee_id.id,
+                contract.id,
+                date_from,
+                date_to
+            )
 
-        # needed so that the shown hours matches any calculations you use them for
-        attendances['number_of_hours'] = round(attendances['number_of_hours'], 2)
+            for day in self.env['hr_timesheet_sheet.sheet.day'].search(valid_days):
+                if day.total_attendance >= 0.0:
+                    attendance['number_of_days'] += 1
+                    attendance['number_of_hours'] += day.total_attendance
 
-        return super(HrPayslip, self).get_worked_day_lines(contract_ids, date_from, date_to) + [attendances]
+            # needed so that the shown hours matches any calculations you use them for
+            attendance['number_of_hours'] = round(attendance['number_of_hours'], 2)
+            attendances.append(attendance)
+
+        return super(HrPayslip, self).get_worked_day_lines(contract_ids, date_from, date_to) + attendances
+
